9e5858fbc019023c0c273952c4d92fbf506ce031
Konstantinos Pascal <pascalkonstantinos@gmail.com>
2020-04-28 09:40:02 +0200

    Delete key request now works

--- frontend/src/components/Keys.js
+++ frontend/src/components/Keys.js
@@ -6,7 +6,7 @@ import MyKeysList from './MyKeysList';
 const Keys = () => {
 	const listMenus = [
 		{
-			menuItem: 'My keys',
+			menuItem: 'My keypairs',
 			render: () => (
 				<Tab.Pane>
 					<MyKeysList />
@@ -14,39 +14,42 @@ const Keys = () => {
 			),
 		},
 		{
-			menuItem: 'User Keys',
+			menuItem: 'User Keypairs',
 			render: () => <Tab.Pane>Tab 2</Tab.Pane>,
 		},
 	];
 
 	return (
-		<Grid stackable columns={2} style={{ margin: '2.5rem' }}>
-			<Grid.Column width={3} style={{ minWidth: '400px' }}>
-				<Tab panes={listMenus} />
-			</Grid.Column>
-			<Grid.Column width={9}>
-				<Grid.Row>
-					<Form>
-						<h3>Select a key to view it</h3>
-						<Form.Group widths='equal'>
-							<Form.TextArea
-								label='Public Key:'
-								placeholder='Public Key'
-								style={{ minHeight: 100 }}
-							/>
-							<Form.TextArea
-								label='Private Key:'
-								placeholder='Private Key'
-								style={{ minHeight: 100 }}
-							/>
-						</Form.Group>
-					</Form>
-				</Grid.Row>
-				<Grid.Row>
-					<h3>Create new key</h3>
-				</Grid.Row>
-			</Grid.Column>
-		</Grid>
+		<div style={{ margin: '2.5rem' }}>
+			<h1>Keys Page</h1>
+			<Grid stackable columns={2}>
+				<Grid.Column width={3} style={{ minWidth: '400px' }}>
+					<Tab panes={listMenus} />
+				</Grid.Column>
+				<Grid.Column width={9}>
+					<Grid.Row>
+						<Form>
+							<h3>Select a keypair to view it</h3>
+							<Form.Group widths='equal'>
+								<Form.TextArea
+									label='Public Key:'
+									placeholder='Public Key'
+									style={{ minHeight: 100 }}
+								/>
+								<Form.TextArea
+									label='Private Key:'
+									placeholder='Private Key'
+									style={{ minHeight: 100 }}
+								/>
+							</Form.Group>
+						</Form>
+					</Grid.Row>
+					<Grid.Row>
+						<h3>Create new keypair</h3>
+					</Grid.Row>
+				</Grid.Column>
+			</Grid>
+		</div>
 	);
 };
 
--- frontend/src/components/MyKeysList.js
+++ frontend/src/components/MyKeysList.js
@@ -5,22 +5,23 @@ import { AuthContext } from '../context/auth-context';
 
 const MyKeysList = () => {
 	const auth = useContext(AuthContext);
+	const authHeader = {
+		headers: {
+			Authorization: auth.token,
+		},
+	};
+
 	const [isLoading, setIsLoading] = useState(false);
 	const [loadedKeys, setLoadedKeys] = useState([]);
 
+	const getUrl = 'http://localhost:8080/api/keys/users/me';
+
+	// GET my all keys request
 	useEffect(() => {
 		const fetchMyKeys = async () => {
 			setIsLoading(true);
-			const response = await Axios.get(
-				'http://localhost:8080/api/keys/users/me',
-				{
-					headers: {
-						Authorization: auth.token,
-					},
-				}
-			)
+			await Axios.get(getUrl, authHeader)
 				.then((response) => {
-					console.log(response.data);
 					setLoadedKeys(response.data.keypairs);
 					setIsLoading(false);
 				})
@@ -31,15 +32,40 @@ const MyKeysList = () => {
 		fetchMyKeys();
 	}, [Axios.get]);
 
+	// DELETE a key request
+	function deleteKey(KeypairID) {
+		const deleteUrl = 'http://localhost:8080/api/keys/' + KeypairID;
+
+		setIsLoading(true);
+		Axios.delete(deleteUrl, authHeader)
+			.then(() => {
+				return Axios.get(getUrl, authHeader);
+			})
+			.then((response) => {
+				const updatedKeys = response.data.keypairs;
+				setLoadedKeys(updatedKeys);
+				setIsLoading(false);
+			})
+			.catch((err) => {
+				console.log(err.response.data);
+			});
+	}
+
 	return (
 		<List as='ul' divided relaxed>
 			{!isLoading &&
 				loadedKeys.map((item) => (
-					<List.Item as='a'>
+					<List.Item as='a' key={item.KeypairID}>
 						<List.Icon name='key' size='large' verticalAlign='middle' />
 						<List.Content>
 							<List.Header>{item.Name}</List.Header>
-							<Button floated='right' size='small' compact negative>
+							<Button
+								floated='right'
+								size='small'
+								compact
+								negative
+								onClick={() => deleteKey(item.KeypairID)}
+							>
 								Delete
 							</Button>
 							<List.Description>ID: {item.KeypairID}</List.Description>
