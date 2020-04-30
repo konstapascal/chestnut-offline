3230f2271a9d5680cc2f8fc18f5248e005e910d6
Konstantinos Pascal <pascalkonstantinos@gmail.com>
2020-04-30 15:59:13 +0200

    AdminPage fixed, now shows results immediately

--- frontend/src/components/Pages/AdminPage.js
+++ frontend/src/components/Pages/AdminPage.js
@@ -1,132 +1,121 @@
-import React, { useState, useEffect, useContext } from "react";
-import Axios from "axios";
+import React, { useState, useEffect, useContext } from 'react';
+import Axios from 'axios';
 import {
-  List,
-  Item,
-  Grid,
-  Input,
-  Icon,
-  Button,
-  Modal,
-  Header,
-  Segment,
-} from "semantic-ui-react";
-import { AuthContext } from "../../context/auth-context";
+	List,
+	Item,
+	Grid,
+	Input,
+	Icon,
+	Button,
+	Modal,
+	Header,
+	Segment,
+} from 'semantic-ui-react';
+import { AuthContext } from '../../context/auth-context';
 
 const AdminPage = () => {
-  const auth = useContext(AuthContext);
-  const [isLoading, setIsLoading] = useState(false);
-  const [loadedUser, setLoadedUsers] = useState([]);
-  const [search, setSearch] = useState("");
-  const [filteredUsers, setFilteredUsers] = useState([]);
-  const [userId, setUserId] = useState("");
-  const [showConfimModal, setShowConfirmModal] = useState(false);
+	const auth = useContext(AuthContext);
+	const [loadedUser, setLoadedUsers] = useState([]);
+	const [filteredUsers, setFilteredUsers] = useState([]);
+	const [search, setSearch] = useState('');
+	const [showConfimModal, setShowConfirmModal] = useState(false);
 
-  const showDeleteWarningHandler = () => {
-    setShowConfirmModal(true);
-    console.log("opened");
-  };
+	const showDeleteWarningHandler = () => {
+		setShowConfirmModal(true);
+	};
 
-  const cancelDeleteHandler = () => {
-    setShowConfirmModal(false);
-    console.log("closed");
-  };
+	const cancelDeleteHandler = () => {
+		setShowConfirmModal(false);
+	};
 
-  console.log("http://localhost:8080/api/users/" + userId);
+	const confirmDeleteHandler = () => {
+		setShowConfirmModal(false);
+	};
 
-  const confirmDeleteHandler = () => {
-    setShowConfirmModal(false);
+	useEffect(() => {
+		const fetchUsers = async () => {
+			await Axios.get('http://localhost:8080/api/users/', {
+				headers: {
+					Authorization: auth.token,
+				},
+			})
+				.then((response) => {
+					setLoadedUsers(response.data.users);
+					setFilteredUsers(response.data.users);
+				})
+				.catch((err) => {
+					console.log(err.response.data);
+				});
+		};
+		fetchUsers();
+	}, []);
 
-    console.log(userId);
-  };
+	useEffect(() => {
+		loadedUser &&
+			setFilteredUsers(
+				loadedUser.filter((user) =>
+					user.Username.toLowerCase().includes(search.toLowerCase())
+				)
+			);
+	}, [search]);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      setIsLoading(true);
-      const response = await Axios.get("http://localhost:8080/api/users/", {
-        headers: {
-          Authorization: auth.token,
-        },
-      })
-        .then((response) => {
-          setLoadedUsers(response.data.users);
-          setIsLoading(false);
-        })
-        .catch((err) => {
-          console.log(err.response.data);
-        });
-    };
-    fetchUsers();
-  }, [Axios.get]);
-
-  useEffect(() => {
-    !isLoading &&
-      loadedUser &&
-      setFilteredUsers(
-        loadedUser.filter((user) =>
-          user.Username.toLowerCase().includes(search.toLowerCase())
-        )
-      );
-  }, [search, loadedUser]);
-
-  return (
-    <div style={{ margin: "2.5rem" }}>
-      <h1>List of users</h1>
-      <h3>Search user:</h3>
-      <Input icon="search" onChange={(e) => setSearch(e.target.value)} />
-      <Segment>
-        <List as="ul" divided>
-          {!isLoading &&
-            loadedUser &&
-            filteredUsers.map((item) => (
-              <List.Item key={setUserId} id={item.ID}>
-                <Item.Content>
-                  <Modal
-                    trigger={
-                      <Button
-                        onClick={showDeleteWarningHandler}
-                        floated="right"
-                        size="small"
-                        negative
-                      >
-                        Delete account
-                      </Button>
-                    }
-                    open={showConfimModal}
-                    onClose={cancelDeleteHandler}
-                    closeIcon
-                  >
-                    <Header
-                      icon="warning sign"
-                      content="Do you want to delete your account?"
-                    />
-                    <Modal.Content>
-                      <p>
-                        There is no way to recover your account after you delete
-                        it. All your data including your key pairs will be
-                        deleted.
-                        <br /> <b>Are you sure?</b>
-                      </p>
-                    </Modal.Content>
-                    <Modal.Actions>
-                      <Button color="red" onClick={cancelDeleteHandler}>
-                        <Icon name="remove" /> No
-                      </Button>
-                      <Button color="green" onClick={confirmDeleteHandler}>
-                        <Icon name="checkmark" /> Yes
-                      </Button>
-                    </Modal.Actions>
-                  </Modal>
-                  <List.Header as="a">{item.Username}</List.Header>
-                  <List.Description as="a">id: {item.ID}</List.Description>
-                  <List.Description as="a">{item.Email}</List.Description>
-                </Item.Content>
-              </List.Item>
-            ))}
-        </List>
-      </Segment>
-    </div>
-  );
+	return (
+		<div style={{ margin: '2.5rem' }}>
+			<h1>List of users</h1>
+			<h3>Search user:</h3>
+			<Input icon='search' onChange={(e) => setSearch(e.target.value)} />
+			<Segment>
+				<List as='ul' divided>
+					{loadedUser &&
+						filteredUsers.map((item) => (
+							<List.Item key={item.ID} id={item.ID}>
+								<Item.Content>
+									<Modal
+										trigger={
+											<Button
+												onClick={showDeleteWarningHandler}
+												floated='right'
+												size='small'
+												negative
+											>
+												Delete account
+											</Button>
+										}
+										open={showConfimModal}
+										onClose={cancelDeleteHandler}
+										closeIcon
+									>
+										<Header
+											icon='warning sign'
+											content='Do you want to delete your account?'
+										/>
+										<Modal.Content>
+											<p>
+												There is no way to recover your account after you delete
+												it. All your data including your key pairs will be
+												deleted.
+												<br /> <b>Are you sure?</b>
+											</p>
+										</Modal.Content>
+										<Modal.Actions>
+											<Button color='red' onClick={cancelDeleteHandler}>
+												<Icon name='remove' /> No
+											</Button>
+											<Button color='green' onClick={confirmDeleteHandler}>
+												<Icon name='checkmark' /> Yes
+											</Button>
+										</Modal.Actions>
+									</Modal>
+									<List.Header as='a'>{item.Username}</List.Header>
+									<List.Description as='a'>id: {item.ID}</List.Description>
+									<List.Description as='a'>{item.Email}</List.Description>
+								</Item.Content>
+							</List.Item>
+						))}
+				</List>
+			</Segment>
+		</div>
+	);
 };
 
 export default AdminPage;
