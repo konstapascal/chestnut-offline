fe80324ca49de8223707385299b6eeecfc9be38a
Abo <abozarafzali11@gmail.com>
2020-05-06 19:17:20 +0200

    Fixed clicking on key icon showing the key

--- frontend/src/components/MyKeysList.js
+++ frontend/src/components/MyKeysList.js
@@ -1,231 +1,265 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Axios from 'axios';
+import React, { useState, useEffect, useContext } from "react";
+import Axios from "axios";
 import {
-	List,
-	Tab,
-	Icon,
-	Button,
-	Modal,
-	Header,
-	Message,
-} from 'semantic-ui-react';
-import { useLocation } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import moment from 'moment';
+   List,
+   Tab,
+   Icon,
+   Button,
+   Modal,
+   Header,
+   Message,
+} from "semantic-ui-react";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+import moment from "moment";
 
-import { AuthContext } from '../context/auth-context';
-import { SelectedKeyContext } from '../context/selected-key-context';
+import { AuthContext } from "../context/auth-context";
+import { SelectedKeyContext } from "../context/selected-key-context";
 
 const MyKeysList = () => {
-	const auth = useContext(AuthContext);
-	const { selectedKey, setSelectedKey } = useContext(SelectedKeyContext);
+   const auth = useContext(AuthContext);
+   const { selectedKey, setSelectedKey } = useContext(SelectedKeyContext);
 
-	const [loadedKeys, setLoadedKeys] = useState([]);
-	const [loadedPublicKeys, setLoadedPublicKeys] = useState([]);
-	const [modalOpen, setModalOpen] = useState(false);
-	const [activeKey, setActiveKey] = useState('');
+   const [loadedKeys, setLoadedKeys] = useState([]);
+   const [loadedPublicKeys, setLoadedPublicKeys] = useState([]);
+   const [modalOpen, setModalOpen] = useState(false);
+   const [activeKey, setActiveKey] = useState("");
 
-	const authHeader = {
-		headers: {
-			Authorization: auth.token,
-		},
-	};
+   const authHeader = {
+      headers: {
+         Authorization: auth.token,
+      },
+   };
 
-	const handleDeleteModalOpen = (modalID) => setModalOpen(modalID);
-	const handleDeleteModalClose = () => setModalOpen(false);
+   const handleDeleteModalOpen = (modalID) => setModalOpen(modalID);
+   const handleDeleteModalClose = () => setModalOpen(false);
 
-	const handleActiveKey = (name, publicKey, privateKey = '') => {
-		setActiveKey(name);
-		setSelectedKey({ PublicKey: publicKey, PrivateKey: privateKey });
-	};
+   const handleActiveKey = (name, publicKey, privateKey = "") => {
+      setActiveKey(name);
+      setSelectedKey({
+         PublicKey: publicKey,
+         PrivateKey: privateKey,
+         Name: name,
+      });
+   };
 
-	const getUrl = 'http://localhost:8080/api/keys/users/me';
-	let location = useLocation();
+   const getUrl = "http://localhost:8080/api/keys/users/me";
+   let location = useLocation();
 
-	// GET all my keys request
-	const fetchMyKeys = () => {
-		Axios.get(getUrl, authHeader)
-			.then((response) => {
-				setLoadedKeys(response.data.keypairs);
-			})
-			.catch((err) => {
-				setLoadedKeys([]);
-				console.log(err.response.data);
-			});
-	};
+   // GET all my keys request
+   const fetchMyKeys = () => {
+      Axios.get(getUrl, authHeader)
+         .then((response) => {
+            setLoadedKeys(response.data.keypairs);
+         })
+         .catch((err) => {
+            setLoadedKeys([]);
+            console.log(err.response.data);
+         });
+   };
 
-	useEffect(() => {
-		fetchMyKeys();
-	}, []);
+   useEffect(() => {
+      fetchMyKeys();
+   }, []);
 
-	// Get saved public keys from localstorage
-	useEffect(() => {
-		const storageKeys = JSON.parse(localStorage.getItem('addedPublicKeys'));
+   // Get saved public keys from localstorage
+   useEffect(() => {
+      const storageKeys = JSON.parse(localStorage.getItem("addedPublicKeys"));
 
-		if (storageKeys === null) {
-			// Make new local storage field if there isn't one
-			return localStorage.setItem('addedPublicKeys', JSON.stringify([]));
-		} else {
-			// If there is local storage data, add it to state
-			setLoadedPublicKeys(storageKeys);
-		}
-	}, []);
+      if (storageKeys === null) {
+         // Make new local storage field if there isn't one
+         return localStorage.setItem("addedPublicKeys", JSON.stringify([]));
+      } else {
+         // If there is local storage data, add it to state
+         setLoadedPublicKeys(storageKeys);
+      }
+   }, []);
 
-	// DELETE a key request
-	const deleteKey = (KeypairID) => {
-		const deleteUrl = 'http://localhost:8080/api/keys/' + KeypairID;
+   // DELETE a key request
+   const deleteKey = (KeypairID) => {
+      const deleteUrl = "http://localhost:8080/api/keys/" + KeypairID;
 
-		Axios.delete(deleteUrl, authHeader)
-			.then(() => {
-				return Axios.get(getUrl, authHeader);
-			})
-			.then((response) => {
-				setLoadedKeys(response.data.keypairs);
-			})
-			.catch((err) => {
-				console.log(err.response.data);
-			});
-	};
+      Axios.delete(deleteUrl, authHeader)
+         .then(() => {
+            return Axios.get(getUrl, authHeader);
+         })
+         .then((response) => {
+            setLoadedKeys(response.data.keypairs);
+         })
+         .catch((err) => {
+            console.log(err.response.data);
+         });
+   };
 
-	// Remove public key from list and local storage
-	const removePublicKey = (KeypairID) => {
-		const newLoadedPublicKeys = loadedPublicKeys.filter(
-			(key) => key.ID !== KeypairID
-		);
+   // Remove public key from list and local storage
+   const removePublicKey = (KeypairID) => {
+      const newLoadedPublicKeys = loadedPublicKeys.filter(
+         (key) => key.ID !== KeypairID
+      );
 
-		setLoadedPublicKeys(newLoadedPublicKeys);
-		localStorage.setItem(
-			'addedPublicKeys',
-			JSON.stringify(newLoadedPublicKeys)
-		);
-	};
+      setLoadedPublicKeys(newLoadedPublicKeys);
+      localStorage.setItem(
+         "addedPublicKeys",
+         JSON.stringify(newLoadedPublicKeys)
+      );
+   };
 
-	const listPanes = [
-		{
-			menuItem: 'My Keypairs',
-			render: () => (
-				<Tab.Pane>
-					<List divided relaxed>
-						{loadedKeys.length === 0 && (
-							<Message>
-								<Icon name='key' size='large' />
-								No keypairs.
-							</Message>
-						)}
+   const listPanes = [
+      {
+         menuItem: "My Keypairs",
+         render: () => (
+            <Tab.Pane>
+               <List divided relaxed>
+                  {loadedKeys.length === 0 && (
+                     <Message>
+                        <Icon name="key" size="large" />
+                        No keypairs.
+                     </Message>
+                  )}
 
-						{loadedKeys.map((item) => (
-							<List.Item
-								as='a'
-								key={item.KeypairID}
-								name={item.Name}
-								active={activeKey === item.Name}
-								onClick={() =>
-									handleActiveKey(item.Name, item.PublicKey, item.PrivateKey)
-								}
-							>
-								<List.Icon name='key' size='large' verticalAlign='middle' />
-								<List.Content>
-									<List.Header>{item.Name}</List.Header>
-									<List.Description>Length: {item.Length}</List.Description>
-									<List.Description>
-										Created:{' '}
-										{moment(item.createdAt)
-											.local()
-											.format('DD MMM YYYY, HH:mm')}
-									</List.Description>
-								</List.Content>
+                  {loadedKeys.map((item) => (
+                     <List.Item
+                        as="a"
+                        key={item.KeypairID}
+                        name={item.Name}
+                        active={activeKey === item.Name}
+                     >
+                        <List.Icon
+                           name="key"
+                           size="large"
+                           verticalAlign="middle"
+                        />
+                        <List.Content
+                           onClick={() =>
+                              handleActiveKey(
+                                 item.Name,
+                                 item.PublicKey,
+                                 item.PrivateKey
+                              )
+                           }
+                        >
+                           <List.Header>{item.Name}</List.Header>
+                           <List.Description>
+                              Length: {item.Length}
+                           </List.Description>
+                           <List.Description>
+                              Created:{" "}
+                              {moment(item.createdAt)
+                                 .local()
+                                 .format("DD MMM YYYY, HH:mm")}
+                           </List.Description>
+                        </List.Content>
 
-								{location.pathname === '/keys' && (
-									<Modal
-										trigger={
-											<List.Icon
-												name='delete'
-												floated='right'
-												size='large'
-												color='red'
-												verticalAlign='middle'
-												negative
-												onClick={() => handleDeleteModalOpen(item.KeypairID)}
-											/>
-										}
-										size='tiny'
-										open={modalOpen === item.KeypairID}
-										onClose={handleDeleteModalClose}
-										closeIcon
-									>
-										<Header icon='delete' color='red' content='Delete key?' />
-										<Modal.Content>
-											<p>
-												Are you sure you want to delete <b>{item.Name}</b>?
-											</p>
-										</Modal.Content>
-										<Modal.Actions>
-											<Button color='red' onClick={handleDeleteModalClose}>
-												<Icon name='remove' /> No
-											</Button>
-											<Button
-												color='green'
-												onClick={() => deleteKey(item.KeypairID)}
-											>
-												<Icon name='checkmark' />
-												Yes
-											</Button>
-										</Modal.Actions>
-									</Modal>
-								)}
-							</List.Item>
-						))}
-					</List>
-				</Tab.Pane>
-			),
-		},
-		{
-			menuItem: 'Public Keys',
-			render: () => (
-				<Tab.Pane>
-					<List divided relaxed>
-						{loadedPublicKeys.length === 0 && (
-							<Message>
-								<Icon name='key' size='large' verticalAlign='middle' />
-								Click <Link to='/users'>here</Link> to add public keys.
-							</Message>
-						)}
-						{loadedPublicKeys.map((key) => (
-							<List.Item
-								as='a'
-								key={key.ID}
-								name={key.keyName}
-								active={activeKey === key.keyName}
-								onClick={() => handleActiveKey(key.keyName, key.publicKey)}
-							>
-								<List.Icon name='key' size='large' verticalAlign='middle' />
-								<List.Content>
-									<List.Header>{key.keyName}</List.Header>
-									<List.Description>
-										Owner: <b>{key.keyOwner}</b>
-									</List.Description>
-									<List.Description>Length: {key.keyLength}</List.Description>
-								</List.Content>
-								{location.pathname === '/keys' && (
-									<List.Icon
-										name='delete'
-										floated='right'
-										size='large'
-										color='red'
-										verticalAlign='middle'
-										negative
-										onClick={() => removePublicKey(key.ID)}
-									/>
-								)}
-							</List.Item>
-						))}
-					</List>
-				</Tab.Pane>
-			),
-		},
-	];
+                        {location.pathname === "/keys" && (
+                           <Modal
+                              trigger={
+                                 <List.Icon
+                                    name="delete"
+                                    floated="right"
+                                    size="large"
+                                    color="red"
+                                    verticalAlign="middle"
+                                    negative
+                                    onClick={() =>
+                                       handleDeleteModalOpen(item.KeypairID)
+                                    }
+                                 />
+                              }
+                              size="tiny"
+                              open={modalOpen === item.KeypairID}
+                              onClose={handleDeleteModalClose}
+                              closeIcon
+                           >
+                              <Header
+                                 icon="delete"
+                                 color="red"
+                                 content="Delete key?"
+                              />
+                              <Modal.Content>
+                                 <p>
+                                    Are you sure you want to delete{" "}
+                                    <b>{item.Name}</b>?
+                                 </p>
+                              </Modal.Content>
+                              <Modal.Actions>
+                                 <Button
+                                    color="red"
+                                    onClick={handleDeleteModalClose}
+                                 >
+                                    <Icon name="remove" /> No
+                                 </Button>
+                                 <Button
+                                    color="green"
+                                    onClick={() => deleteKey(item.KeypairID)}
+                                 >
+                                    <Icon name="checkmark" />
+                                    Yes
+                                 </Button>
+                              </Modal.Actions>
+                           </Modal>
+                        )}
+                     </List.Item>
+                  ))}
+               </List>
+            </Tab.Pane>
+         ),
+      },
+      {
+         menuItem: "Public Keys",
+         render: () => (
+            <Tab.Pane>
+               <List divided relaxed>
+                  {loadedPublicKeys.length === 0 && (
+                     <Message>
+                        <Icon name="key" size="large" verticalAlign="middle" />
+                        Click <Link to="/users">here</Link> to add public keys.
+                     </Message>
+                  )}
+                  {loadedPublicKeys.map((key) => (
+                     <List.Item
+                        as="a"
+                        key={key.ID}
+                        name={key.keyName}
+                        active={activeKey === key.keyName}
+                     >
+                        <List.Icon
+                           name="key"
+                           size="large"
+                           verticalAlign="middle"
+                        />
+                        <List.Content
+                           onClick={() =>
+                              handleActiveKey(key.keyName, key.publicKey)
+                           }
+                        >
+                           <List.Header>{key.keyName}</List.Header>
+                           <List.Description>
+                              Owner: <b>{key.keyOwner}</b>
+                           </List.Description>
+                           <List.Description>
+                              Length: {key.keyLength}
+                           </List.Description>
+                        </List.Content>
+                        {location.pathname === "/keys" && (
+                           <List.Icon
+                              name="delete"
+                              floated="right"
+                              size="large"
+                              color="red"
+                              verticalAlign="middle"
+                              negative
+                              onClick={() => removePublicKey(key.ID)}
+                           />
+                        )}
+                     </List.Item>
+                  ))}
+               </List>
+            </Tab.Pane>
+         ),
+      },
+   ];
 
-	return <Tab panes={listPanes} />;
+   return <Tab panes={listPanes} />;
 };
 
 export default MyKeysList;
--- frontend/src/components/Pages/KeysPage.js
+++ frontend/src/components/Pages/KeysPage.js
@@ -1,50 +1,51 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { Form, Grid } from 'semantic-ui-react';
+import React, { useContext, useState, useEffect } from "react";
+import { Form, Grid } from "semantic-ui-react";
 
-import MyKeysList from '../MyKeysList';
-import GenerateKey from '../GenerateKey';
+import MyKeysList from "../MyKeysList";
+import GenerateKey from "../GenerateKey";
 
-import { SelectedKeyContext } from '../../context/selected-key-context';
+import { SelectedKeyContext } from "../../context/selected-key-context";
 
 const KeysPage = () => {
-	const { selectedKey } = useContext(SelectedKeyContext);
+   const { selectedKey } = useContext(SelectedKeyContext);
 
-	return (
-		<div style={{ margin: '2.5rem' }}>
-			<h1>Keys Page</h1>
-			<Grid stackable columns={2}>
-				<Grid.Column width={3} style={{ minWidth: '400px' }}>
-					<MyKeysList />
-				</Grid.Column>
-				<Grid.Column width={9}>
-					<Grid.Row>
-						<Form>
-							<h3>Select a keypair to view it</h3>
-							<Form.Group widths='equal'>
-								<Form.TextArea
-									value={selectedKey.PublicKey}
-									label='Public Key:'
-									placeholder='Public Key'
-									style={{ minHeight: 100 }}
-									disabled
-								/>
-								<Form.TextArea
-									value={selectedKey.PrivateKey}
-									label='Private Key:'
-									placeholder='Private Key'
-									style={{ minHeight: 100 }}
-									disabled
-								/>
-							</Form.Group>
-						</Form>
-					</Grid.Row>
-					<Grid.Row>
-						<GenerateKey />
-					</Grid.Row>
-				</Grid.Column>
-			</Grid>
-		</div>
-	);
+   return (
+      <div style={{ margin: "2.5rem" }}>
+         <h1>Keys Page</h1>
+         <Grid stackable columns={2}>
+            <Grid.Column width={3} style={{ minWidth: "400px" }}>
+               <MyKeysList />
+            </Grid.Column>
+            <Grid.Column width={9}>
+               <Grid.Row>
+                  <Form>
+                     <h3>Select a keypair to view it</h3>
+                     <h5>Selected key: {selectedKey.Name}</h5>
+                     <Form.Group widths="equal">
+                        <Form.TextArea
+                           value={selectedKey.PrivateKey}
+                           label="Private Key:"
+                           placeholder="Private Key"
+                           style={{ minHeight: 100 }}
+                           // disabled
+                        />
+                        <Form.TextArea
+                           value={selectedKey.PublicKey}
+                           label="Public Key:"
+                           placeholder="Public Key"
+                           style={{ minHeight: 100 }}
+                           // disabled
+                        />
+                     </Form.Group>
+                  </Form>
+               </Grid.Row>
+               <Grid.Row>
+                  <GenerateKey />
+               </Grid.Row>
+            </Grid.Column>
+         </Grid>
+      </div>
+   );
 };
 
 export default KeysPage;
