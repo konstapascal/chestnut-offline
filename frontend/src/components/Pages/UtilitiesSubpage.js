0df45c8775993744898c6fd2d8692b22773174f2
Abo <abozarafzali11@gmail.com>
2020-05-24 17:35:52 +0200

    uuid fixed

--- frontend/src/components/Pages/UtilitiesSubpage.js
+++ frontend/src/components/Pages/UtilitiesSubpage.js
@@ -2,46 +2,46 @@ import React from 'react';
 import { Tab } from 'semantic-ui-react';
 import Base64Encoding from '../Base64Encoding';
 import Base64Decoding from '../Base64Decoding';
-import UuidGenerator from '../UuidGenerator';
+import UUIDGenerator from '../UUIDGenerator';
 import Checksum from '../Checksum';
 
 const UtilitiesSubpage = () => {
-	const UtilitiesOptions = [
-		{
-			menuItem: 'Base64 Encoding',
-			render: () => (
-				<Tab.Pane>
-					<Base64Encoding />
-				</Tab.Pane>
-			),
-		},
-		{
-			menuItem: 'Base64 Decoding',
-			render: () => (
-				<Tab.Pane>
-					<Base64Decoding />
-				</Tab.Pane>
-			),
-		},
-		{
-			menuItem: 'UUID Generator',
-			render: () => (
-				<Tab.Pane>
-					<UuidGenerator />
-				</Tab.Pane>
-			),
-		},
-		{
-			menuItem: 'Checksum',
-			render: () => (
-				<Tab.Pane>
-					<Checksum />
-				</Tab.Pane>
-			),
-		},
-	];
+   const UtilitiesOptions = [
+      {
+         menuItem: 'Base64 Encoding',
+         render: () => (
+            <Tab.Pane>
+               <Base64Encoding />
+            </Tab.Pane>
+         ),
+      },
+      {
+         menuItem: 'Base64 Decoding',
+         render: () => (
+            <Tab.Pane>
+               <Base64Decoding />
+            </Tab.Pane>
+         ),
+      },
+      {
+         menuItem: 'UUID Generator',
+         render: () => (
+            <Tab.Pane>
+               <UUIDGenerator />
+            </Tab.Pane>
+         ),
+      },
+      {
+         menuItem: 'Checksum',
+         render: () => (
+            <Tab.Pane>
+               <Checksum />
+            </Tab.Pane>
+         ),
+      },
+   ];
 
-	return <Tab panes={UtilitiesOptions} />;
+   return <Tab panes={UtilitiesOptions} />;
 };
 
 export default UtilitiesSubpage;
