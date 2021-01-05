e0a32c11395e71f89d5aa3910563275bb48d18df
konstapascal <pascalkonstantinos@gmail.com>
2021-01-06 00:38:15 +0100

    Updates

---
+++ precache-manifest.7cc65a373d45e39e64d8e2c749a2b24f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c470045d03755bc6d4c4e0e7012e3e1",
+    "url": "/chestnut-offline/index.html"
+  },
+  {
+    "revision": "e6cb0ed1922250947056",
+    "url": "/chestnut-offline/static/css/2.0fcc0773.chunk.css"
+  },
+  {
+    "revision": "e6cb0ed1922250947056",
+    "url": "/chestnut-offline/static/js/2.c7a2df51.chunk.js"
+  },
+  {
+    "revision": "af77bd59492cc9db2d4521e6b8025f10",
+    "url": "/chestnut-offline/static/js/2.c7a2df51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c50ec51d84f3d47e15a",
+    "url": "/chestnut-offline/static/js/main.dc378503.chunk.js"
+  },
+  {
+    "revision": "d5f1a3520abdcc52bbd8",
+    "url": "/chestnut-offline/static/js/runtime-main.c2e1fb51.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/chestnut-offline/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/chestnut-offline/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/chestnut-offline/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/chestnut-offline/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/chestnut-offline/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "1a8f3e3ec687f56363ca072c514e62fb",
+    "url": "/chestnut-offline/static/media/chestnut.1a8f3e3e.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/chestnut-offline/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/chestnut-offline/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/chestnut-offline/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/chestnut-offline/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/chestnut-offline/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/chestnut-offline/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/chestnut-offline/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/chestnut-offline/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/chestnut-offline/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/chestnut-offline/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/chestnut-offline/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
